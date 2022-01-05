f7b6d77a4d5d5ef64682a2b106ef0f8e7d74764a
TamakiTatsuki0525 <tamakitatsukiworking@gmail.com>
2022-01-05 16:57:21 +0900

    タイルマップのアセットを作成、ステージと仮の移動スクリプトを入れて、TileMapの動作を確認した。

---
+++ Assets/Scripts/Test/InputManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    protected static readonly string[] findTags =
+    {
+        "InputManager",
+    };
+
+    private static InputManager instance;
+
+    public static InputManager Instance {
+        get
+        {
+            if (instance == null)
+            {
+
+                Type type = typeof(InputManager);
+
+                foreach (var tag in findTags)
+                {
+                    GameObject[] objs = GameObject.FindGameObjectsWithTag(tag);
+
+                    for (int j = 0; j < objs.Length; j++)
+                    {
+                        instance = (InputManager)objs[j].GetComponent(type);
+                        if (instance != null)
+                            return instance;
+                    }
+                }
+
+                Debug.LogWarning(string.Format("{0} is not found", type.Name));
+            }
+
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        CheckInstance();
+    }
+
+    private bool CheckInstance()
+    {
+        if (instance == null)
+        {
+            instance = (InputManager)this;
+            DontDestroyOnLoad(gameObject);
+            return true;
+        }
+        else if (Instance == this)
+        {
+            return true;
+        }
+
+        Destroy(this);
+        return false;
+    }
+
+    /* -- Horizontal“ü—Í --------------------------------------------------------------------------- */
+    private float moveKey = 0;
+    public float MoveKey {
+        get { return moveKey; }
+    }
+
+    /* -- Jump“ü—Í --------------------------------------------------------------------------------- */
+    private int jumpKey = 0;
+    public int JumpKey {
+        get { return jumpKey; }
+    }
+
+    void Update()
+    {
+        // ˆÚ“®
+        moveKey = Input.GetAxisRaw("Horizontal");
+        // ƒWƒƒƒ“ƒv
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpKey = 1;
+        }
+        else if (Input.GetButton("Jump"))
+        {
+            jumpKey = 2;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            jumpKey = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Test/PlayerController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    private Vector2 localScale;
+    private bool isGrounded = true;
+    private bool isJumping = false;
+    private bool isJumpingCheck = true;
+    private float jumpTimeCounter;
+    private float jumpTime = 0.35f;
+    private float _jumpPower;
+    [SerializeField] private LayerMask platformLayer;
+    [SerializeField] private float testY = 0.55f;
+    public GUIStyle textStyle;
+
+
+    InputManager inputManager;
+    PlayerManager playerManager;
+
+    void Awake()
+    {
+        localScale = transform.localScale;
+        jumpTimeCounter = jumpTime;
+    }
+
+    void Start()
+    {
+        playerManager = PlayerManager.Instance;
+        inputManager = InputManager.Instance;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(50, 50, 50, 50), "isGrounded" + isGrounded, textStyle);
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.Linecast(transform.position - transform.up * 0.1f, transform.position - transform.up * testY, platformLayer);
+        Debug.DrawLine(transform.position - transform.up * 0.1f, transform.position - transform.up * testY, Color.red, 0, false);
+    }
+
+    void FixedUpdate()
+    {
+        if (inputManager.MoveKey != 0)
+        {
+            // Œü‚«‚ð•Ï‚¦‚é
+            localScale.x = inputManager.MoveKey * localScale.x;
+            transform.localScale = localScale;
+        }
+
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(inputManager.MoveKey * playerManager.MoveSpeed, rb.velocity.y);
+
+            if (isJumpingCheck && inputManager.JumpKey != 0)
+            {
+                jumpTimeCounter = jumpTime;
+                isJumpingCheck = false;
+                isJumping = true;
+                _jumpPower = playerManager.JumpPower;
+            }
+        }
+        else
+        {
+            if (inputManager.JumpKey == 0)
+            {
+                isJumping = false;
+            }
+            if (!isJumping)
+            {
+                rb.velocity = new Vector2(inputManager.MoveKey * playerManager.JumpMoveSpeed, Physics.gravity.y * playerManager.GravityRate);
+            }
+        }
+
+        if (isJumping)
+        {
+            jumpTimeCounter -= Time.deltaTime;
+
+            if (inputManager.JumpKey == 2)
+            {
+                _jumpPower -= 0.2f;
+                rb.velocity = new Vector2(inputManager.MoveKey * playerManager.JumpMoveSpeed, 1 * _jumpPower);
+            }
+            if (jumpTimeCounter < 0)
+            {
+                isJumping = false;
+            }
+        }
+
+        if (inputManager.JumpKey == 0)
+        {
+            isJumpingCheck = true;
+        }
+    }
+}
